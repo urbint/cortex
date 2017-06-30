24a64babfedf7c7002a623ff83dba32b17b61db4
Ryan Schmukler <rschmukler@gmail.com>
2017-06-30 15:50:51 -0400

    feat(config): allow disabling of cortex via config (#5)

--- lib/cortex/application.ex
+++ lib/cortex/application.ex
@@ -8,30 +8,64 @@ defmodule Cortex.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
     cond do
       Mix.env in [:dev, :test] ->
-        children = [
-          FileWatcher.child_spec,
-          worker(Reloader, []),
-          worker(Controller, [])
-        ]
-
-        env_specific_children =
-          case Mix.env do
-            :dev ->
-              []
-            :test ->
-              [worker(TestRunner, [])]
+        children =
+          if enabled?() do
+            children()
+          else
+            []
           end
 
-        opts = [strategy: :one_for_one, name: Cortex.Supervisor]
-
-        Supervisor.start_link(children ++ env_specific_children, opts)
+        Supervisor.start_link(children, strategy: :one_for_one, name: Cortex.Supervisor)
 
       true ->
         {:error, "Only :dev and :test environments are allowed"}
+    end
+  end
+
+  defp children() do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      FileWatcher.child_spec,
+      worker(Reloader, []),
+      worker(Controller, [])
+    ]
+
+    env_specific_children =
+      case Mix.env do
+        :dev ->
+          []
+        :test ->
+          [worker(TestRunner, [])]
+      end
+
+    children ++ env_specific_children
+  end
+
+
+  defp enabled? do
+    case Application.get_env(:cortex, :enabled, true) do
+      bool when is_boolean(bool) ->
+        bool
+      {:system, env_var, default} ->
+        get_system_var(env_var, default)
+      invalid ->
+        raise "Invalid config value for Cortex `:enabled`: #{inspect invalid}"
+    end
+  end
+
+  defp get_system_var(env_var, default) do
+    case System.get_env(env_var) do
+      nil ->
+        default
+      truthy when truthy in ["YES", "yes", "true", "TRUE", "1"] ->
+        true
+      falsey when falsey in ["NO", "no", "false", "FALSE", "0"] ->
+        false
+      _ ->
+        raise "Unparsable Cortex Environment Variable '#{env_var}'"
     end
   end
 end
