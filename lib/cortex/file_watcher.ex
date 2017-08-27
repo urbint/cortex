b07165dfb22aab14a7a6a744cb5fa969ea0f773d
Xiangrong Hao <falood@gmail.com>
2017-08-27 15:53:38 -0700

    refactor(file_watcher): updated to use new file_system api
    * use file_system as file watcher
    
    * make file_watcher a standalone module
    
    * updated to file_system v0.2

--- lib/cortex/file_watcher.ex
+++ lib/cortex/file_watcher.ex
@@ -3,15 +3,36 @@ defmodule Cortex.FileWatcher do
 
   alias Cortex.Controller
 
-  use ExFSWatch, dirs: ["lib/", "test/", "apps/"] |> Enum.filter(&File.dir?/1)
+  use GenServer
 
+  require Logger
 
-  def callback(:stop), do: :ok
-
-  def callback(path, _events) do
-    GenServer.cast(Controller, {:file_changed, file_type(path), path})
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
   end
 
+  def init(_) do
+    dirs = ["lib/", "test/", "apps/"] |> Enum.filter(&File.dir?/1)
+    {:ok, watcher_pid} = FileSystem.start_link(dirs: dirs)
+    FileSystem.subscribe(watcher_pid)
+    {:ok, %{watcher_pid: watcher_pid}}
+  end
+
+
+  def handle_info({:file_event, watcher_pid, {path, _events}}, %{watcher_pid: watcher_pid}=state) do
+    GenServer.cast(Controller, {:file_changed, file_type(path), path})
+    {:noreply, state}
+  end
+
+  def handle_info({:file_event, watcher_pid, :stop}, %{watcher_pid: watcher_pid}=state) do
+    Logger.info "File watcher stopped."
+    {:noreply, state}
+  end
+
+  def handle_info(data, state) do
+    Logger.info "Get unexcepted message #{inspect data}, ignore..."
+    {:noreply, state}
+  end
 
   def file_type(path) do
     is_elixir_file? =
