b155f16c5d49040ea848ad0a48803f89c2695ce4
Griffin Smith <root@gws.fyi>
2017-09-19 15:29:01 -0400

    Release 0.4.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cortex.Mixfile do
 
   def project do
     [app: :cortex,
-     version: "0.3.1",
+     version: "0.4.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
