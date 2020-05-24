b2897306c6cda6683c80b3d4b24cbcdfd5bedd60
Jason Axelson <jason.axelson@gmail.com>
2020-05-23 18:41:29 -1000

    Fix Code.load_file deprecation
    Code.load_file was deprecated in Elixir
    1.10 (https://hexdocs.pm/elixir/compatibility-and-deprecations.html)
    
    This replaces it with `Code.compile_file/1` (`Code.require_file/1` was not used
    because we want to be able to recompile the same file multiple times).
    
    Since `Code.compile_file/1` was added in Elixir 1.7, support for 1.6 was
    removed. 1.7 was released in July 2018 so it is almost 2 years old at this
    point, so hopefully it is okay to drop support.
    
    https://elixir-lang.org/blog/2018/07/25/elixir-v1-7-0-released/

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cortex.Mixfile do
     [
       app: :cortex,
       version: "0.6.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/urbint/cortex",
